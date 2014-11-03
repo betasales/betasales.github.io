f272303a4baf9a896f8cd88cd3c706c8d7ff3161
adamdaigian <adam.daigian@gmail.com>
2014-11-03 09:36:10 -0800

    adding all the files now

--- dist/js/eb.js
+++ dist/js/eb.js
@@ -14,83 +14,16 @@ $(document).ready(function() {
     $('#signup-form').show()
   });  
 
-  //use cases jquery  
-  $('#use-case-1').click(function () {
-    $('#use-case-select-1').addClass('active')
-    $('#use-case-carrot-1').addClass('active')
-    $('#use-case-lead-1').addClass('active')
-    $('#use-case-description-1').show()
-    $('#use-case-select-2').removeClass('active')
-    $('#use-case-carrot-2').removeClass('active')
-    $('#use-case-lead-2').removeClass('active')
-    $('#use-case-description-2').hide()
-    $('#use-case-select-3').removeClass('active')
-    $('#use-case-carrot-3').removeClass('active')
-    $('#use-case-lead-3').removeClass('active')
-    $('#use-case-description-3').hide()
-    $('#use-case-select-4').removeClass('active')
-    $('#use-case-carrot-4').removeClass('active')
-    $('#use-case-lead-4').removeClass('active')
-    $('#use-case-description-4').hide()
-  });
-  $('#use-case-2').click(function () {
-    $('#use-case-select-2').addClass('active')
-    $('#use-case-carrot-2').addClass('active')
-    $('#use-case-lead-2').addClass('active')
-    $('#use-case-description-2').show()
-    $('#use-case-select-1').removeClass('active')
-    $('#use-case-carrot-1').removeClass('active')
-    $('#use-case-lead-1').removeClass('active')
-    $('#use-case-description-1').hide()
-    $('#use-case-select-3').removeClass('active')
-    $('#use-case-carrot-3').removeClass('active')
-    $('#use-case-lead-3').removeClass('active')
-    $('#use-case-description-3').hide()
-    $('#use-case-select-4').removeClass('active')
-    $('#use-case-carrot-4').removeClass('active')
-    $('#use-case-lead-4').removeClass('active')
-    $('#use-case-description-4').hide()
-  });
-  $('#use-case-3').click(function () {
-    $('#use-case-select-3').addClass('active')
-    $('#use-case-carrot-3').addClass('active')
-    $('#use-case-lead-3').addClass('active')
-    $('#use-case-description-3').show()
-    $('#use-case-select-1').removeClass('active')
-    $('#use-case-carrot-1').removeClass('active')
-    $('#use-case-lead-1').removeClass('active')
-    $('#use-case-description-1').hide()
-    $('#use-case-select-2').removeClass('active')
-    $('#use-case-carrot-2').removeClass('active')
-    $('#use-case-lead-2').removeClass('active')
-    $('#use-case-description-2').hide()
-    $('#use-case-select-4').removeClass('active')
-    $('#use-case-carrot-4').removeClass('active')
-    $('#use-case-lead-4').removeClass('active')
-    $('#use-case-description-4').hide()
-  });
-  $('#use-case-4').click(function () {
-    $('#use-case-select-4').addClass('active')
-    $('#use-case-carrot-4').addClass('active')
-    $('#use-case-lead-4').addClass('active')
-    $('#use-case-description-4').show()
-    $('#use-case-select-1').removeClass('active')
-    $('#use-case-carrot-1').removeClass('active')
-    $('#use-case-lead-1').removeClass('active')
-    $('#use-case-description-1').hide()
-    $('#use-case-select-2').removeClass('active')
-    $('#use-case-carrot-2').removeClass('active')
-    $('#use-case-lead-2').removeClass('active')
-    $('#use-case-description-2').hide()
-    $('#use-case-select-3').removeClass('active')
-    $('#use-case-carrot-3').removeClass('active')
-    $('#use-case-lead-3').removeClass('active')
-    $('#use-case-description-3').hide()
-  });
-
-  //data sheet download
-  $('#datasheet-prompt').click(function () { 
-    $('#datasheet-prompt').hide()
-    $('#download-datasheet-filter').show()
-  });
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(window).height()){
+            $(".navbar-default").css({"border-bottom":"solid 1px #edeeef"});
+            $(".navbar-default").css({"background-color":"#FFFFFF"});
+        }
+        else {
+            $(".navbar-default").css({"background-color":"transparent"});
+        }
+    })
+})
