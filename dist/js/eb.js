b4cef69e8aa64722b40830535dc1b48eabf0e2f6
adamdaigian <adam.daigian@gmail.com>
2014-11-12 22:30:31 -0800

    badass navbar brah

--- dist/js/eb.js
+++ dist/js/eb.js
@@ -12,18 +12,33 @@ $(document).ready(function() {
     $('#email-signup').hide()
     $('.divider').hide()
     $('#signup-form').show()
-  });  
+  }); 
+
+  jQuery(function() {
+    var menu = jQuery(".navbar.navbar-default.navbar-fixed-top");
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 15) {
+         console.log('hello')
+         menu.addClass("navbar-fill");
+    } else {
+        menu.removeClass("navbar-fill");
+        }
+    });
+  }); 
 
 });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".navbar-default").css({"border-bottom":"solid 1px #edeeef"});
-            $(".navbar-default").css({"background-color":"#FFFFFF"});
-        }
-        else {
-            $(".navbar-default").css({"background-color":"transparent"});
-        }
-    })
-})
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if($(window).scrollTop() > $(window).height()){
+//             $(".navbar-default").css({"border-bottom":"solid 1px #edeeef"});
+//             $(".navbar-default").css({"background-color":"#FFFFFF"});
+//         }
+//         else {
+//             $(".navbar-default").css({"background-color":"transparent"});
+//         }
+//     })
+// })
+
+// Menu Animation
